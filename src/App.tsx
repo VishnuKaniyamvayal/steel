a536e427a3875d253cf75849ab10dcc61c8774af
vishnu vinod <vishnujishnu0987@gmail.com>
2026-07-13 23:54:44 +0530

    dark test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <main className="dark flex min-h-screen items-center justify-center bg-neutral-950 p-8">
+    <main className="flex min-h-screen items-center justify-center p-8">
       <Button variant="steel" size="xl">Send</Button>
     </main>
   );
