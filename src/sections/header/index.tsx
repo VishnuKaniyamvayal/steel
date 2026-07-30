6e6b559f4e77859a2942ea3e5f9313f897885e5c
vishnu vinod <vishnujishnu0987@gmail.com>
2026-07-30 22:51:18 +0530

    initialize button added to header

--- src/sections/header/index.tsx
+++ src/sections/header/index.tsx
@@ -1,3 +1,13 @@
+import { FolderGit2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 export function Header() {
-	return <div className="h-15.25 border-b">sample header</div>;
+	return (
+		<header className="flex h-15.25 items-center justify-end border-b px-4">
+			<Button size={"xs"} variant="outline">
+					<FolderGit2 className="" />
+				Initialize git
+			</Button>
+		</header>
+	);
 }
